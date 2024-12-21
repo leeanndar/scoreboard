a3b82e878ea0d10787138ba9d867f04bd4b27fdf
Leeann <leeanndar525@gmail.com>
2024-12-21 18:15:47 -0500

    timer js

--- index.js
+++ index.js
@@ -61,4 +61,48 @@ function clearQuarter() {
 
 
 
+const display = document.getElementById("timer-el")
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
 
+function timerStart() {
+    if (!isRunning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
+    }
+}
+
+function timerStop() {
+    if(isRunning){
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRunning = false;
+    }
+}
+
+function timerReset() {
+    clearInterval(timer);
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00";
+}
+
+function update() {
+    
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    display.textContent = `${minutes}:${seconds}:${milliseconds}`
+}
